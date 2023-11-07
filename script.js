866c756beb3949639e51f3bcf8a3330c6fccac72
Dgraham23 <100248168+Dgraham23@users.noreply.github.com>
2023-11-06 21:29:41 -0800

    commit 3

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 
-
+// Adds new task from add task input
 function newElement() {
-    var inputValue = document.getElementById("input").value;
+    const inputValue = document.getElementById("taskInput").value;
     if (inputValue === '') {
         alert("You must write something!");
     } else {
-        var taskContainer = document.querySelector('.taskContainer');
-        var newTask = document.createElement('div');
+        const tasksFrame = document.querySelector('.tasksFrame');
+        const newTask = document.createElement('div');
+        newTask.classList.add('taskContainer');
+        
+        const elements = document.getElementsByClassName('taskContainer');
+        const count = elements.length;
+
+        if (count > 0) {
+            newTask.setAttribute('id', count+1);
+        } else {
+            newTask.setAttribute('id', '1');
+        }
+
         newTask.innerHTML = '<h3>' + inputValue + '</h3>';
-        taskContainer.appendChild(newTask);
+        tasksFrame.appendChild(newTask);
     }
-    document.getElementById("input").value = "";
+    
+}
+
+
+
+function expandTasks() {
+    const taskContainers = document.getElementsByClassName('taskContainer');
+
+    for (let i = 0; i < taskContainers.length; i++) {
+        taskContainers[i].onclick = function () {
+            const clickedId = taskContainers[i].id;
+            for (let j = 0; j < taskContainers.length; j++) {
+                if (taskContainers[j].id !== clickedId) {
+                    taskContainers[j].style.display = 'none';
+                }
+            }
+        };
+    }
+
+
 }
