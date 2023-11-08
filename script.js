266ab7b4968bbd335f9a603bf951edb8fa526c8c
Dgraham23 <100248168+Dgraham23@users.noreply.github.com>
2023-11-07 20:11:41 -0800

    commit 4
    Added functions for hide div and show div on reclick

--- script.js
+++ script.js
@@ -1,44 +1,65 @@
+// Hide other divs that have class taskContainer
+document.addEventListener('DOMContentLoaded', function() {
+    const tasksFrame = document.querySelector('.tasksFrame');
 
-// Adds new task from add task input
+    tasksFrame.addEventListener('click', function(event) {
+
+        if (event.target.classList.contains('expanded')) {
+            document.querySelectorAll('.taskContainer').forEach(container => {
+                container.style.display = '';
+                container.classList.remove('expanded');
+            })
+        }
+
+        else {
+        if (event.target.classList.contains('taskContainer')) {
+            const clickedId = event.target.id;
+
+            document.querySelectorAll('.taskContainer').forEach(container => {
+                if (container.id !== clickedId) {
+                    container.style.display = 'none';
+                } if (container.id == clickedId) {
+                    container.classList.add('expanded');
+                }
+            });
+        }
+
+            // console.log(`Comment logged: The div with ID ${clickedId} was clicked!`);      
+        }
+    });
+});
+
+
+
+// Function to add a new element
 function newElement() {
     const inputValue = document.getElementById("taskInput").value;
-    if (inputValue === '') {
+    if (!inputValue) {
         alert("You must write something!");
     } else {
         const tasksFrame = document.querySelector('.tasksFrame');
         const newTask = document.createElement('div');
         newTask.classList.add('taskContainer');
-        
-        const elements = document.getElementsByClassName('taskContainer');
-        const count = elements.length;
 
-        if (count > 0) {
-            newTask.setAttribute('id', count+1);
-        } else {
-            newTask.setAttribute('id', '1');
-        }
+        const count = document.querySelectorAll('.taskContainer').length;
+        newTask.setAttribute('id', count + 1);
 
-        newTask.innerHTML = '<h3>' + inputValue + '</h3>';
+        newTask.innerHTML = `<h3>${inputValue}</h3>`;
         tasksFrame.appendChild(newTask);
-    }
-    
-}
 
+        // Attach the event listener to the newly created element
+        newTask.addEventListener('click', function(event) {
+            const clickedId = event.currentTarget.id;
 
-
-function expandTasks() {
-    const taskContainers = document.getElementsByClassName('taskContainer');
-
-    for (let i = 0; i < taskContainers.length; i++) {
-        taskContainers[i].onclick = function () {
-            const clickedId = taskContainers[i].id;
-            for (let j = 0; j < taskContainers.length; j++) {
-                if (taskContainers[j].id !== clickedId) {
-                    taskContainers[j].style.display = 'none';
+            document.querySelectorAll('.taskContainer').forEach(container => {
+                if (container.id !== clickedId) {
+                    container.style.display = 'none';
+                } if (container.id == clickedId) {
+                    container.classList.add('expanded');
                 }
-            }
-        };
+            });
+
+            // console.log(`Comment logged: The div with ID ${clickedId} was clicked!`);
+        });
     }
-
-
 }
